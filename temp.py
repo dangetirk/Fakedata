0ea32abbeee74c444dde0ee419e8c607e0da2c2c
Ratna77 <113950889+Ratna77@users.noreply.github.com>
2023-04-02 00:07:55 +0100

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,10 +1,32 @@
-from simple_salesforce import Salesforce
+import csv
+import subprocess
+import json
+from datetime import datetime
 
-# Connect to Salesforce using the authorization information
-sf = Salesforce(instance_url='https://your_instance.salesforce.com', session_id='your_session_id')
+# Replace with your Salesforce credentials and object name
+username = 'YOUR_SALESFORCE_USERNAME'
+object_name = 'YOUR_SALESFORCE_OBJECT_NAME'
 
-# Run a SOQL query to get data from a table
-query = "SELECT Id, Name, Email FROM Contact"
-contacts = sf.query(query)
+# Authenticate with Salesforce using sfdx
+auth_info = json.loads(subprocess.check_output(['sfdx', 'force:org:display', '-u', username, '-j']).decode('utf-8'))
+access_token = auth_info['result']['accessToken']
+instance_url = auth_info['result']['instanceUrl']
 
-# Do something with the query results...
+# Query data from the object using the Salesforce API
+headers = {
+    'Authorization': 'Bearer ' + access_token,
+    'Content-Type': 'application/json'
+}
+query = 'SELECT Id, Name, CreatedDate FROM {}'.format(object_name)
+url = instance_url + '/services/data/v51.0/query/?q=' + query
+result = json.loads(requests.get(url, headers=headers).text)['records']
+
+# Write data to CSV file
+filename = '{}_{}.csv'.format(object_name, datetime.now().strftime('%Y%m%d_%H%M%S'))
+with open(filename, 'w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=result[0].keys())
+    writer.writeheader()
+    for row in result:
+        writer.writerow(row)
+
+print('Data written to file:', filename)
