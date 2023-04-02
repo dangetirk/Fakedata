baec653ec902287200d58ba8c56827461c6930e9
Ratna77 <113950889+Ratna77@users.noreply.github.com>
2023-04-02 14:21:15 +0100

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,53 +1,24 @@
-import csv
-import subprocess
-import requests
-from datetime import datetime
-import sys
+from dbcomparer import DbComparer
+from simple_salesforce import Salesforce
+from google.cloud import bigquery
 
-# Replace with your Salesforce credentials
-username = 'YOUR_SALESFORCE_USERNAME'
+# Connect to Salesforce
+sf = Salesforce(username='<your_username>', password='<your_password>', security_token='<your_security_token>')
 
-# Get the object name from the command line arguments
-if len(sys.argv) < 2:
-    print('Usage: python salesforce_to_csv.py <object_name>')
-    sys.exit(1)
-object_name = sys.argv[1]
+# Connect to BigQuery
+client = bigquery.Client()
 
-# Authenticate with Salesforce using sfdx
-try:
-    auth_info = subprocess.check_output(['sfdx', 'force:org:display', '-u', username, '--json']).decode('utf-8')
-    auth_info = json.loads(auth_info.strip())
-    access_token = auth_info['result']['accessToken']
-    instance_url = auth_info['result']['instanceUrl']
-except Exception as e:
-    print('Error authenticating with Salesforce:', e)
-    sys.exit(1)
+# Retrieve data from Salesforce
+sf_query = 'SELECT Id, Name, Email FROM Contact'
+sf_data = sf.query(sf_query)['records']
 
-# Query data from the object using the Salesforce API
-try:
-    headers = {
-        'Authorization': 'Bearer ' + access_token,
-        'Content-Type': 'application/json'
-    }
-    query = 'SELECT * FROM {}'.format(object_name)
-    url = instance_url + '/services/data/v51.0/query/?q=' + query
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
-    result = json.loads(response.text)['records']
-except Exception as e:
-    print('Error querying Salesforce:', e)
-    sys.exit(1)
+# Retrieve data from BigQuery
+bq_query = 'SELECT id, name, email FROM mydataset.contacts'
+bq_data = client.query(bq_query).to_dataframe()
 
-# Write data to CSV file
-try:
-    filename = '{}_{}.csv'.format(object_name, datetime.now().strftime('%Y%m%d_%H%M%S'))
-    with open(filename, 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(result[0].keys())
-        for row in result:
-            writer.writerow(row.values())
+# Compare the data
+comparer = DbComparer()
+result = comparer.compare(sf_data, bq_data)
 
-    print('Data written to file:', filename)
-except Exception as e:
-    print('Error writing data to CSV file:', e)
-    sys.exit(1)
+# Print the result
+print(result)
